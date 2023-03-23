05f699b7cccad39b1108fb5bbe39a13e73482812
Illia <93096896+b1gzzz@users.noreply.github.com>
2023-03-24 00:29:54 +0200

    Add more tests
    Remove test reports
    
    wip

--- SelzyAutotest/src/test/java/SelzyLogin.java
+++ SelzyAutotest/src/test/java/SelzyLogin.java
@@ -2,10 +2,36 @@ import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.closeWindow;
+import static org.openqa.selenium.By.xpath;
 
 public class SelzyLogin {
+    @Test
+    public void emailExists() {
+        open("https://cp.selzy.com/en/v5/login");
+        $(xpath("/html/body/div[1]/div[3]/div[2]/div/div/div/div/div/div/form/div[2]/div/div[2]/input"))
+                .should(Condition.exist);
+    }
+
+    @Test
+    public void passwordExists() {
+        open("https://cp.selzy.com/en/v5/login");
+        $(xpath("/html/body/div[1]/div[3]/div[2]/div/div/div/div/div/div/form/div[3]/div[1]/div/div[2]/input"))
+                .should(Condition.exist);
+    }
+
+    @Test
+    public void onlineChat() {
+        open("https://cp.selzy.com/en/v5/login");
+        $(xpath("/html/body/div[1]/div[3]/div[3]/div/button"))
+                .click();
+        $(xpath("/html/body/div[5]/div/div[1]"))
+                .should(Condition.exist);
+    }
+
     @Test
     public void signUpPage() {
 
@@ -15,5 +41,16 @@ public class SelzyLogin {
 
     }
 
+    @Test
+    public void logIn() {
+        open("https://cp.selzy.com/en/v5/login");
+        $(xpath("/html/body/div[1]/div[3]/div[2]/div/div/div/div/div/div/form/div[2]/div/div[2]/input"))
+                .setValue("depsilidri@gufum.com");
+        $(xpath("/html/body/div[1]/div[3]/div[2]/div/div/div/div/div/div/form/div[3]/div[1]/div/div[2]/input"))
+                .setValue("qwertyu");
+        $(byText("Log in")).click();
+        $(byText("Let's keep in touch")).should(Condition.exist);
+        closeWindow();
+    }
 
 }
